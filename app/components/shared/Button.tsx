34e954bc875efd776014e4788bab7e71901e43dd
muneeb ur rehman <muneebrehman123.pak@gmail.com>
2024-10-30 12:18:19 +0500

    changes

--- app/components/shared/Button.tsx
+++ app/components/shared/Button.tsx
@@ -2,7 +2,7 @@ import { FC } from "react"
 const Button: FC<{ text: string }> = ({ text }) => {
     return (
         <div>
-            <button className="bg-[#00616C] text-white font-medium text-lg md:text-xl px-5 md:px-8 h-12 md:h-14
+            <button className="z-20 bg-[#00616C] text-white font-medium text-lg md:text-xl px-5 md:px-8 h-12 md:h-14
             rounded-full  hover:shadow-lg  hover:bg-[#0c7985] hover:scale-105 duration-500">{text}</button>
         </div>
     )
--- app/components/widget/CoreTracks.tsx
+++ app/components/widget/CoreTracks.tsx
@@ -21,20 +21,19 @@ const CoreTracks = () => {
             description: "$-101: Dollar Making Bootcamp - Full-Stack Template and API Product Development",
             number: 3
         }
-        
+
     ]
 
     return (
         <section className="mt-16 lg:mt-28">
-            
+
             <Wrapper>
-            <div className="absolute left-0 hidden sm:block ">
-            <div className="blur-[150px] -z-20 bg-primary w-64 h-64 rounded-full "></div></div>
+                
                 <div className="max-w-screen-2xl">
                     <h4 className="text-teal-700 font-bold text-base md:text-lg">Program of Studies</h4>
                     <h2 className="text-2xl md:text-4xl font-bold whitespace-pre-line">{heading}</h2>
                     <p className="mt-4 text-base md:text-lg text-zinc-600 ">Every participant of the program will start by completing the following three core courses.</p>
-                    <div className="mt-6">
+                    <div className="mt-6 z-20">
                         <Button text={"Enoll Now"} />
                     </div>
                 </div>
@@ -45,38 +44,17 @@ const CoreTracks = () => {
                         QCardsData.map((items) => {
 
                             return (
-                                <QuarterCard 
-                                key={items.number} 
-                                heading={items.quarter} 
-                                description={items.description} 
-                                number={items.number}
-                                WidthDivider="basis-1/12 md:basis-4/12" />
-                            
-                        )
-                    })
+                                <QuarterCard
+                                    key={items.number}
+                                    heading={items.quarter}
+                                    description={items.description}
+                                    number={items.number}
+                                    WidthDivider="basis-1/12 md:basis-4/12" />
+
+                            )
+                        })
                     }
-                    {/* <div className="absolute bg-green-500  rounded-[100px] w[200px] h-[200px] z-="></div> */}
-                    {/* <QuarterCard heading="QuarterI"p="CS-101: Object-Oriented Programming using TypeScript"number={1}/>
-                    <QuarterCard heading="QuarterII"p="CS-101: Object-Oriented Programming using TypeScript"number={2}/>
-                    <QuarterCard heading="QuarterIII"p="CS-101: Object-Oriented Programming using TypeScript"number={3}/> */}
-                    {/* <div className=" relative border rounded-md  w-4/12 px-8 py-12">
-                        <h4 className="font-bold text-lg">Quarter I</h4>
-                        <p className="mt-2 text-slate-600 ">CS-101: Object-Oriented Programming using TypeScript</p>
-                        <div className="absolute top-1 right-14   text-gray-300 text-9xl font-bold -z-10">1</div>
-                    </div>
-
-                    <div className=" border rounded-md bg-green-300 w-4/12 px-8 py-8">
-                        <h4 className="font-bold text-lg">Quarter II</h4>
-                        <p className="text-slate-600 ">CS-101: Object-Oriented Programming using TypeScript</p>
-                        <span>2</span>
-                    </div>
-
-                    <div className=" border rounded-md bg-green-300 w-4/12 px-8 py-8">
-                        <h4 className="font-bold text-lg">Quarter III</h4>
-                        <p className="text-slate-600 ">CS-101: Object-Oriented Programming using TypeScript</p>
-                        <span>3</span>
-                    </div>
-                    <QuarterCard text={"QuarterIV"}text2={" CS-101: Object-Oriented Programming using TypeScript 4"}text3={4}/> */}
+                   
                 </div>
             </Wrapper>
         </section >
